e1cf17268944f2c09cfa30626854e0bcb64a4d9f
Gregory Sobotka <gregorysobotka@gmail.com>
2020-10-05 10:13:22 -0700

    including brewfile

--- brew/brew-cask-installs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-echo "brew cask install"
-brew cask install $(< $CUSTOM_IMPORT_PATH/brew/brew-cask-installs.txt )
--- brew/brew-installs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/zsh
-echo "brew install"
-brew install $(< $CUSTOM_IMPORT_PATH/brew/brew-installs.txt )
