c1f7b9253e8accfc6889f818a1f3b81bc0e9d1b2
Gregory Sobotka <gregorysobotka@gmail.com>
2020-10-12 14:37:44 -0700

    removing exports

--- zsh/ohmyzsh-custom/load.zsh
+++ zsh/ohmyzsh-custom/load.zsh
@@ -8,18 +8,6 @@ DOTFILEPATH=$CUSTOM_IMPORT_PATH/dotfiles
 CUSTOM_LOG_FILES=$CUSTOM_IMPORT_PATH/logs
 ### Custom Paths (end)
 
-# Java
-# export OPENJDK="/usr/local/opt/openjdk/bin"
-
-# kubernetes
-# source <(kubectl completion zsh)
-# complete -F __start_kubectl k
-
-### Python (start)
-# pyenv
-eval "$(pyenv init -)"
-### Python (end)
-
 # Golang
 export GOPATH=$HOME/go
 export GOBIN=$GOPATH/bin
@@ -29,9 +17,13 @@ export JAVABIN=${JAVA_HOME}/bin
 export OPENJDK=/usr/local/opt/openjdk/bin
 # Python
 export POETRYBIN=$HOME/.poetry/bin
+eval "$(pyenv init -)"
 # NVM
 export NVM_DIR=~/.nvm 
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+# kubernetes
+# source <(kubectl completion zsh)
+# complete -F __start_kubectl k
 
 PATH=$PATH:$GOPATH:$GOBIN:$JAVABIN:$POETRYBIN:$OPENJDK
 
